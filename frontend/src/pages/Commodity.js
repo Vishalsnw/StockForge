00b1ab59933e0b4d488a4c6c5750dc025321d9a5
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-06-15 15:50:40 +0530

    Create Commodity.js

---
+++ frontend/src/pages/Commodity.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CommodityMarket from "../components/CommodityMarket";
+
+export default function Commodity() {
+  return (
+    <div style={{ maxWidth: 700, margin: "2rem auto" }}>
+      <CommodityMarket />
+    </div>
+  );
+}
