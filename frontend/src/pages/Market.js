8543843467c960f506cbb3becbffde1b8d04e7f7
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-06-15 15:49:31 +0530

    Create Market.js

---
+++ frontend/src/pages/Market.js
@@ -0,0 +1,10 @@
+import React from "react";
+import MarketPanel from "../components/MarketPanel";
+
+export default function Market() {
+  return (
+    <div>
+      <MarketPanel />
+    </div>
+  );
+}
