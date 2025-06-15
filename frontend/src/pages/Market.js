69b378c800ca2e98b5390d6ad365818435613fe0
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-06-15 21:31:28 +0530

    Add files via upload

--- frontend/src/pages/Market.js
+++ frontend/src/pages/Market.js
@@ -1,10 +1,14 @@
 import React from "react";
-import MarketPanel from "../components/MarketPanel";
+import MarketTicker from "../components/MarketTicker";
+import "./MarketPage.css";
 
 export default function Market() {
   return (
-    <div>
-      <MarketPanel />
+    <div className="market-page">
+      <MarketTicker />
+      <h1>Market Overview</h1>
+      <p>Track live prices of all stocks and commodities. Click on Stock or Commodity above to trade.</p>
+      {/* Optionally add a summary table here */}
     </div>
   );
 }
