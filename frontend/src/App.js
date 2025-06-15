e67033fc5e6d51716e39708718167e2b95f36378
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-06-15 22:39:14 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { MarketBotProvider } from "./bots/MarketBot";
+import { UserProvider } from "./context/UserContext";
 import NavigationBar from "./components/NavigationBar";
 import Home from "./pages/Home";
 import Company from "./pages/Company";
@@ -14,24 +15,26 @@ import "./App.css";
 function App() {
   return (
     <Router>
-      <MarketBotProvider>
-        <div className="app-layout">
-          <NavigationBar />
-          <div className="app-content">
-            <main className="main-panel">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/company" element={<Company />} />
-                <Route path="/market" element={<Market />} />
-                <Route path="/stock" element={<StockMarket />} />
-                <Route path="/commodity" element={<CommodityMarket />} />
-                <Route path="/leaderboard" element={<Leaderboard />} />
-                <Route path="/profile" element={<Profile />} />
-              </Routes>
-            </main>
+      <UserProvider>
+        <MarketBotProvider>
+          <div className="app-layout">
+            <NavigationBar />
+            <div className="app-content">
+              <main className="main-panel">
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/company" element={<Company />} />
+                  <Route path="/market" element={<Market />} />
+                  <Route path="/stock" element={<StockMarket />} />
+                  <Route path="/commodity" element={<CommodityMarket />} />
+                  <Route path="/leaderboard" element={<Leaderboard />} />
+                  <Route path="/profile" element={<Profile />} />
+                </Routes>
+              </main>
+            </div>
           </div>
-        </div>
-      </MarketBotProvider>
+        </MarketBotProvider>
+      </UserProvider>
     </Router>
   );
 }
