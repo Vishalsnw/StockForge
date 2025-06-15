78b3f3ebd79b85cb00fa7d520dbe8418e1a5efbc
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-06-15 15:48:38 +0530

    Create Company.js

---
+++ frontend/src/pages/Company.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CompanyPanel from "../components/CompanyPanel";
+
+export default function Company() {
+  return (
+    <div>
+      <CompanyPanel />
+    </div>
+  );
+}
